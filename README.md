9af9c84b0fc7bc7e55142d752860229cb6f3a5cd
kelesss <122500471+SUPERkelesss@users.noreply.github.com>
2025-09-26 17:51:53 +0800

    assets 3

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# superkelesss.github.io
-kelesss的个人网站
+# 凯楽斯Kelesssの聖教园地(superkelesss.github.io)
+kelesss的个人网站，喵呜♪(\^∇\^*)
+
+网站发布：[GitHub Pages](https://docs.github.com/en/pages)
+
+网站构建：[MkDocs](https://mkdocs.org.cn/)
+
+网页美化：[Material for MkDocs](https://squidfunk.github.io/mkdocs-material/)
+
+公式：[MathJax](https://www.mathjax.org/)
+
---
+++ docs/javascripts/mathjax.js
@@ -0,0 +1,21 @@
+window.MathJax = {
+  loader: {load: ['[tex]/physics']},
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+    packages: {'[+]': ['physics']}
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => { 
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
